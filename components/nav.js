581f8e9ea16cca38a1de27ae6a0fe6bfe3ca071a
antonioortegajr <antonioortegajr@gmail.com>
2020-04-25 20:08:37 -0700

    phone number fix

--- components/nav.js
+++ components/nav.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'tel:541-654-563', label: '541-654-563', icon: 'fa fa-phone'},
+  { href: 'tel:541-654-5632', label: '541-654-5632', icon: 'fa fa-phone'},
   { href: 'https://www.google.com/maps/search/?api=1&query=level+up+arcade', label: '1290 Oak Street, Eugene, OR 97401', icon: 'fa fa-map-marker'},
   { href: 'https://www.facebook.com/leveluparcade', label: 'leveluparcade', icon: 'fa fa-facebook'},
 ].map(link => ({
